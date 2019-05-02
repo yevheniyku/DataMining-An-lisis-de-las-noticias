9bff39b7c9bcbcab7c69e97aa3673f2bd9eab46d
Unknown <ykushch@ucm.es>
2019-05-02 22:17:39 +0200

    limpieza final

--- aux-scripts/cleaning.py
+++ aux-scripts/cleaning.py
@@ -1,17 +1,35 @@
 import xml.etree.ElementTree as ET
 import io
-
+import unidecode
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+swords = stopwords.words('spanish')
+
+def addCustomStopwords():
+    global swords
+    print(len(swords))
+    with open('../data/custom-stopwords.csv', 'r', encoding='utf8') as f:
+        for line in f:
+            new_word = []
+            new_word.append(unidecode.unidecode(line.strip()))
+            swords.extend(new_word)
+    print(len(swords))
+
 def cleanWords(line):
-    stop_words = set(stopwords.words('spanish'))
+    global swords
     words = line.split()
+    cont = 1
     newLine = ''
     for r in words:
-        if not r in stop_words:
-            if r.isalpha():
+        r = unidecode.unidecode(r)
+        if not r in swords and r.isalpha():
+            if cont == 8:
+                newLine = newLine + ' ' + r + '\n'
+                cont = 1
+            else:
                 newLine = newLine + ' ' + r
+                cont += 1
     return newLine
 
 def cleanText(text):
@@ -27,17 +45,17 @@ def cleanText(text):
 
 def getText(root):
     print('Empezamos a sacar y limpiar el texto...')
-    with open('./data/articlesText70.txt', 'a', encoding='utf8') as file:
+    with open('../data/cleanWords1970.txt', 'a', encoding='utf8') as file:
         for child in root:
             ct = cleanText(child[2].text)
             file.write(ct)
 
-
 def main():
     print('Inicio del script...')
-    tree = ET.parse('./data/elpais1970.xml')
+    tree = ET.parse('../data/elpais1970.xml')
     root = tree.getroot()
 
+    addCustomStopwords()
     getText(root)
 
 if __name__ == '__main__':
